7546238ef1b6b30c673be8816ddc689261d691ff
rooryyy154 <rooryyy154@gmail.com>
2026-06-29 21:12:54 -0700

    second commit

--- server.py
+++ server.py
@@ -67,6 +67,38 @@ def register():
     }), 201
 
 
+#PUT
+@app.put("/api/users/<int:user_id>")
+def update_user(user_id):
+    updated_user = request.get_json()
+
+    connection = sqlite3.connect(DB_NAME)
+    cursor = connection.cursor()
+    cursor.execute("SELECT username, password FROM users WHERE id = ?", (user_id,))
+    user = cursor.fetchone()
+    print(user)
+
+    if not user:
+        connection.close()
+        return jsonify({
+            "success": False,
+            "message": "User not found"
+        }), 404
+
+    username = updated_user.get("username", user[0])
+    password = updated_user.get("password", user[1])
+
+    cursor.execute("UPDATE users SET username = ?, password = ? WHERE id = ?", (username, password, user_id))
+    
+    connection.commit()
+    connection.close()
+
+    return jsonify({
+            "success": True,
+            "message": "User updated successfully"
+        }), 200
+
+
 # http://127.0.0.1:5000/api/users
 @app.get("/api/users")
 def get_users():
@@ -90,6 +122,61 @@ def get_users():
     }),200
 
 
+@app.get("/api/users/<int:user_id>") #path parameter <>
+def get_user_by_id(user_id):
+    connection = sqlite3.connect(DB_NAME)
+    connection.row_factory = sqlite3.Row #you need it if you want to use dict row
+    cursor = connection.cursor()
+    cursor.execute("SELECT id, username FROM users WHERE id = ?", (user_id,)) #SQL lite is 
+    row = cursor.fetchone()
+    print(row)
+    connection.close()
+
+    if not row: #bettwer way to use if not to check it.
+        return jsonify({
+            "success": False,
+            "message": "User not found"
+        }), 404
+
+    user = dict(row)
+
+    return jsonify({
+        "success": True,
+        "message": "User retrieved succesfully",
+        "data": user
+
+    }), 200
+
+
+#PUT
+
+
+#DELETE
+@app.delete("/api/users/<int:user_id>")
+def delete_user_by_id(user_id):
+    connection = sqlite3.connect(DB_NAME)
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT id,username FROM users WHERE id = ?", (user_id,))
+    user = cursor.fetchone()
+
+    if not user:
+        connection.close()
+        return jsonify({
+            "success": False,
+            "message": "User not found"
+        }), 404
+
+    cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
+    connection.commit()
+    connection.close()
+
+    return jsonify({
+        "success": True,
+        "message": "User deleted succesfully"
+    }), 200
+
+
 @app.post("/api/expenses")
 def create_expense():
     new_expense = request.get_json()
@@ -101,6 +188,14 @@ def create_expense():
     date_expense = new_expense.get("date", date.today())
     category = new_expense.get("category","")
     user_id = new_expense.get("user_id", 0)
+
+    allowed_categories = ["Food", "Entertainment", "Education"]
+    if category not in allowed_categories:
+        return jsonify({
+            "success": False,
+            "message": "Invalid category"
+        }), 400
+    
 
     connection = sqlite3.connect(DB_NAME)
     cursor = connection.cursor()
@@ -136,6 +231,96 @@ def get_expenses():
         "message": "Expenses retrieved succesfully",
         "data": expenses
     })
+
+@app.get("/api/expenses/<int:expense_id>")
+def get_expense_by_id(expense_id):
+    connection = sqlite3.connect(DB_NAME)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM expenses WHERE id = ?", (expense_id,))
+    row = cursor.fetchone()
+    connection.close()
+
+    if not row:
+        return jsonify({
+            "success": False,
+            "message": "Expense not found"
+        }), 404
+
+    return jsonify({
+        "success": True,
+        "message": "Expense found succesfully",
+        "data": dict(row)
+    }), 200
+
+# PUT /api/expenses/<expense_id>
+@app.put("/api/expenses/<int:expense_id>")
+def update_expense(expense_id):
+    updated_expense = request.get_json()
+    print(updated_expense)
+
+    if "category" in updated_expense and updated_expense["category"] not in ["Food", "Education", "Entertainment"]:
+        return jsonify({
+            "success": False,
+            "message": "Invalid category."
+        }), 400
+
+    connection = sqlite3.connect(DB_NAME)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM expenses WHERE id = ?", (expense_id,))
+    expense = cursor.fetchone()
+
+    if not expense:
+        connection.close()
+        return jsonify({
+            "success": False,
+            "message": "Expense not found"
+        }), 404
+
+    title = updated_expense.get("title", expense["title"])
+    description = updated_expense.get("description", expense["description"])
+    amount = updated_expense.get("amount", expense["amount"])
+    category = updated_expense.get("category", expense["category"])
+    user_id = updated_expense.get("user_id", expense["user_id"])
+
+    cursor.execute("""
+        UPDATE expenses
+        SET title = ?, description = ?, amount = ?, category = ?, user_id = ?
+        WHERE id = ?""", (title, description, amount, category, user_id, expense_id))
+    connection.commit()
+    connection.close()
+
+    return jsonify({
+        "success": True,
+        "message": "Expense updated successfully"
+    }), 200
+
+
+# DELETE /api/expenses/<expense_id>
+@app.delete("/api/expenses/<int:expense_id>")
+def delete_expense(expense_id):
+    connection = sqlite3.connect(DB_NAME)
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT id FROM expenses WHERE id = ?", (expense_id,))
+    expense = cursor.fetchone()
+
+    if not expense:
+        connection.close()
+        return jsonify({
+            "success": False,
+            "message": "Expense not found"
+        }), 404
+
+    cursor.execute("DELETE FROM expenses WHERE id = ?", (expense_id,))
+    connection.commit()
+    connection.close()
+
+    return jsonify({
+        "success": True,
+        "message": "Expense deleted succesfully"
+    }), 200
 
 
 if __name__ == "__main__":
